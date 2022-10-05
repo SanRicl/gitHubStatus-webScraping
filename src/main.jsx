d2c4e45c47c49c7e8917287e0646a2a79da43e51
sanricl <sandersonricl@gmail.com>
2022-10-04 22:27:20 -0300

    chore: update structure app

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import ReactTooltip from "react-tooltip";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ReactTooltip delayHide={1000}/>
     <App />
   </React.StrictMode>
 );
